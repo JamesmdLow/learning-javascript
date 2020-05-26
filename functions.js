e77379db21a3fcab793f43528b9cb33a929bb756
James Low <jamesmdlow@gmail.com>
2020-05-26 21:43:31 +1000

    trying js

--- functions.js
+++ functions.js
@@ -5,5 +5,9 @@ function multiply(a, b) {
 function multiply_logging(a, b) {
     console.log(a * b);
 }
+function subtract(a,b) {
+    console.log(a - b);
+}
 
 multiply_logging(1, 55)
+subtract(1, 2)
