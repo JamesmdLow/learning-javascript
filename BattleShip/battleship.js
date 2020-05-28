0fda44b1d7582e21de89ba95ac274040a8e7623d
Calvin Pang <cal.pang123@gmail.com>
2020-05-28 13:47:46 +1000

    fixed

--- BattleShip/battleship.js
+++ BattleShip/battleship.js
@@ -6,17 +6,16 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk == false) {
+while (!isSunk) {
     guess = prompt("Ready, aim, fire! (enter a number from 0-6:");
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
     } else {
-        guesses = guesses + 1;
-
-        if (guess == location1 || location2 || guess == location3) {
+        guesses++;
+        if (guess == location1 || guess == location2 || guess == location3) {
             alert("HIT!");
-            hits = hits + 1;
-            if (hits == 3) {
+            hits++;
+            if (hits === 3) {
                 isSunk = true;
                 alert("You sank my battleship!");
             }
@@ -25,6 +24,7 @@ while (isSunk == false) {
         }
     }
 }
-var stats = "You took" + guesses + "guesses to sink the battleship," + "which means your shooting accuracy was" + (3/guesses);
+
+var stats = "You took " + guesses + "guesses to sink the battleship," + "which means your shooting accuracy was" + (3/guesses);
 alert(stats);
 
