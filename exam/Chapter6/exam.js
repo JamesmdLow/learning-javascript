40ff2e5539d225e2e8e1bae1c580b1e631e1ecf1
Calvin Pang <cal.pang123@gmail.com>
2020-06-15 21:36:43 +1000

    Chapter 6 exam

---
+++ exam/Chapter6/exam.js
@@ -0,0 +1,9 @@
+//  Question 1. What is the difference between javascript and html? What does what?
+//  Question 2. What does DOM stand for? What is the document object and what is it used for?
+//  Question 3. Open up exam.html and write some code to change the word 'James you are a' to 'James you are a freak!'
+
+// hint: use the document.getElementById() function to change the innerText
+
+
+// Question 4: What does the following stand for in html and explain its usage
+// <ul> <ol> <a> <p> <body> <head> <html> <script>
